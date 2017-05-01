7f208f1c3d0cb5313bb4370841fe37377335c191
Chris Beer <chris@cbeer.info>
2017-05-01 13:47:09 -0700

    Pass url options through to blacklight url helpers

--- app/controllers/concerns/blacklight/controller.rb
+++ app/controllers/concerns/blacklight/controller.rb
@@ -87,7 +87,7 @@ module Blacklight::Controller
 
   def search_action_path *args
     if args.first.is_a? Hash
-      args.first[:only_path] = true
+      args.first[:only_path] = true if args.first[:only_path].nil?
     end
 
     search_action_url(*args)
@@ -101,7 +101,7 @@ module Blacklight::Controller
 
   def search_facet_path(options = {})
     Deprecation.silence(Blacklight::Controller) do
-      search_facet_url(options.merge(only_path: true))
+      search_facet_url(options.reverse_merge(only_path: true))
     end
   end
 
